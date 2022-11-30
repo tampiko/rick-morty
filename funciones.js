8ea7f6cafb8431aa4c2abefd51887edb8642d636
Juan Manuel Herrera Carrera <juan.herrera@cobatab.edu.mx>
2022-11-30 16:30:45 -0600

    Muestra pagina acutal y conteo Pero no genera nada al darle el back ni inicio

--- funciones.js
+++ funciones.js
@@ -6,7 +6,8 @@ function getData(tipo, page = 1) {
   console.log(`getData => ${tipo} | page => ${page}`);
   // tipo => character || location || episode
   let pagina;
-  page != 1 ? (pagina = `?page=${page}`) : (pagina = "");
+  page != 1 ? (pagina = `?page=${page}`) : (pagina = ``);
+  console.log(`Url => ${url.base}/${tipo}${pagina}`);
   return fetch(`${url.base}/${tipo}${pagina}`)
     .then((response) => response.json())
     .then((data) => data);
@@ -18,85 +19,112 @@ const limpiaDiv = () => {
 };
 
 const creaBotones = (info) => {
-  console.log(info);
-  // count: 826
-  // next: "https://rickandmortyapi.com/api/character?page=2"
-  // pages: 42
-  // prev: null
-
-  // Busca Siguiente Pagina
-  console.log(info.next);
+  console.log(`Crea botones`);
+  console.table(info);
+  let bs = document.querySelector("#divBotones");
+  bs.innerHTML = "";
   let posicionPagina = 0;
   let siguientePagina = 0;
-  if (info.next) {
-    console.log("entro");
-    posicionPagina = info.next.indexOf("?");
-    siguientePagina = info.next.substring(posicionPagina + 6);
-  }
+  let paginaAnterior = 1;
+  let posicionTipo;
+  let tipo = "";
+
+  info.next
+    ? (posicionPagina = info.next.indexOf("?"))
+    : (posicionPagina = info.prev.indexOf("?"));
 
   // Busca Tipo
-  let posicionTipo = info.next.indexOf("api/") + 4;
-  let tipo = info.next.substring(posicionTipo, posicionPagina);
-
-  let bs = document.querySelector("#divBotones");
-  // paginaAnterior
-
-  // ultimaPagina
+  if (info.next) {
+    console.log("tiene next");
+    posicionTipo = info.next.indexOf("api/") + 4;
+    tipo = info.next.substring(posicionTipo, posicionPagina);
+    siguientePagina = info.next.substring(posicionPagina + 6);
+    console.log(`== siguientePagina == > ${siguientePagina}`);
+  } else {
+    console.log("tiene prev");
+    posicionTipo = info.prev.indexOf("api/") + 4;
+    tipo = info.prev.substring(posicionTipo, posicionPagina);
+    paginaAnterior = info.prev.substring(posicionPagina + 6);
+    console.log(`== paginaAnterior == > ${paginaAnterior}`);
+  }
 
   // Boton Inicio
-  let btnInicio = document.createElement("button");
-  btnInicio.className = "btn btn-primary";
-  btnInicio.innerHTML = `&leftarrowtail; Inicio`;
-  btnInicio.onclick = function () {
-    getData(tipo, 1);
-  };
+  if (info.prev) {
+    let btnInicio = document.createElement("button");
+    btnInicio.className = "btn btn-primary";
+    btnInicio.innerHTML = `&leftarrowtail; Inicio`;
+    btnInicio.onclick = function () {
+      getData(tipo);
+    };
+    bs.appendChild(btnInicio);
+  }
 
   // Boton Anterior
-  let btnAnterior = document.createElement("button");
-  btnAnterior.className = "btn btn-outline-primary";
-  btnAnterior.innerHTML = `&leftarrow; Anterior`;
-  // btnAnterior.onclick = function(){getData(tipo,paginaAnterior)}
+  if (info.prev) {
+    let btnAnterior = document.createElement("button");
+    btnAnterior.className = "btn btn-outline-primary";
+    btnAnterior.innerHTML = `&leftarrow; Anterior`;
+    btnAnterior.onclick = function () {
+      getData(tipo, paginaAnterior);
+    };
+    bs.appendChild(btnAnterior);
+  }
+
+  // Pagina Actual
+  let paginaActual = siguientePagina - 1;
+  console.log(`paginaActual => ${paginaActual}`);
+  let mostrandoInicio = (paginaActual - 1) * 20 + 1;
+  let mostrandoFin = mostrandoInicio + 19;
 
   let Label = document.createElement("span");
   Label.className = "ms-2 me-2";
-  Label.innerHTML = `Pagina 2 (Mostrando 15 al 30 de ${info.count})`;
+  Label.innerHTML = `Pagina ${paginaActual} (Mostrando del ${mostrandoInicio} al ${mostrandoFin} de ${info.count})`;
+  bs.appendChild(Label);
 
   // Boton Siguiente
-  let btnSiguiente = document.createElement("button");
-  btnSiguiente.className = "btn btn-outline-primary";
-  btnSiguiente.innerHTML = "Siguiente &rightarrow;";
-  btnSiguiente.onclick = function () {
-    generaPagina(tipo, siguientePagina);
-  };
+  if (info.next) {
+    let btnSiguiente = document.createElement("button");
+    btnSiguiente.className = "btn btn-outline-primary";
+    btnSiguiente.innerHTML = "Siguiente &rightarrow;";
+    btnSiguiente.onclick = function () {
+      generaPagina(tipo, siguientePagina);
+    };
+    bs.appendChild(btnSiguiente);
+  }
 
   // Boton Final
-  let btnFinal = document.createElement("button");
-  btnFinal.className = "btn btn-primary";
-  btnFinal.innerHTML = "Final &rightarrowtail;";
-  btnFinal.onclick = function () {
-    generaPagina(tipo, info.pages);
-  };
-  bs.innerHTML = "";
-  bs.appendChild(btnInicio);
-  bs.appendChild(btnAnterior);
-  bs.appendChild(Label);
-  bs.appendChild(btnSiguiente);
-  bs.appendChild(btnFinal);
+  if (info.next) {
+    let btnFinal = document.createElement("button");
+    btnFinal.className = "btn btn-primary";
+    btnFinal.innerHTML = "Final &rightarrowtail;";
+    btnFinal.onclick = function () {
+      generaPagina(tipo, info.pages);
+    };
+    bs.appendChild(btnFinal);
+  }
 };
 
 function hola(nombre) {
   console.log("hola ", nombre);
 }
 
-const generaPagina = (tipo, page = 1) => {
+const generaPagina = (tipo, page) => {
+  console.log(`tipo => ${tipo}`);
+  console.log(`page => ${page}`);
+  if (!page) {
+    page = 1;
+  }
   getData(tipo, page).then((data) => {
+    console.log(`data => ${data}`);
     limpiaDiv();
     data.results.forEach((personaje) => {
       creaPersonajeCard(personaje);
     });
+    console.log(`manda crear botones`);
     creaBotones(data.info);
   });
 };
+
 const creaPersonajeCard = (personaje) => {
   let div = document.createElement("div");
   div.className = "col";
@@ -113,4 +141,5 @@ const creaPersonajeCard = (personaje) => {
   let bs = document.querySelector("#lista");
   bs.appendChild(div);
 };
+
 export { generaPagina };
