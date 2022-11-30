775342b53188f9282f9c3ccb5bcebfc60db149ce
Juan Manuel Herrera Carrera <juan.herrera@cobatab.edu.mx>
2022-11-30 15:17:39 -0600

    trae los datos, y funciona el next.

--- funciones.js
+++ funciones.js
@@ -2,26 +2,115 @@ let url = {
   base: "https://rickandmortyapi.com/api",
 };
 
-let getPersonajesData = (callback) => {
-  fetch(`${url.base}/character`)
+function getData(tipo, page = 1) {
+  console.log(`getData => ${tipo} | page => ${page}`);
+  // tipo => character || location || episode
+  let pagina;
+  page != 1 ? (pagina = `?page=${page}`) : (pagina = "");
+  return fetch(`${url.base}/${tipo}${pagina}`)
     .then((response) => response.json())
-    .then((data) => {
-      callback(data);
+    .then((data) => data);
+}
+
+const limpiaDiv = () => {
+  let bs = document.querySelector("#lista");
+  bs.innerHTML = "";
+};
+
+const creaBotones = (info) => {
+  console.log(info);
+  // count: 826
+  // next: "https://rickandmortyapi.com/api/character?page=2"
+  // pages: 42
+  // prev: null
+
+  // Busca Siguiente Pagina
+  console.log(info.next);
+  let posicionPagina = 0;
+  let siguientePagina = 0;
+  if (info.next) {
+    console.log("entro");
+    posicionPagina = info.next.indexOf("?");
+    siguientePagina = info.next.substring(posicionPagina + 6);
+  }
+
+  // Busca Tipo
+  let posicionTipo = info.next.indexOf("api/") + 4;
+  let tipo = info.next.substring(posicionTipo, posicionPagina);
+
+  let bs = document.querySelector("#divBotones");
+  // paginaAnterior
+
+  // ultimaPagina
+
+  // Boton Inicio
+  let btnInicio = document.createElement("button");
+  btnInicio.className = "btn btn-primary";
+  btnInicio.innerHTML = `&leftarrowtail; Inicio`;
+  btnInicio.onclick = function () {
+    getData(tipo, 1);
+  };
+
+  // Boton Anterior
+  let btnAnterior = document.createElement("button");
+  btnAnterior.className = "btn btn-outline-primary";
+  btnAnterior.innerHTML = `&leftarrow; Anterior`;
+  // btnAnterior.onclick = function(){getData(tipo,paginaAnterior)}
+
+  let Label = document.createElement("span");
+  Label.className = "ms-2 me-2";
+  Label.innerHTML = `Pagina 2 (Mostrando 15 al 30 de ${info.count})`;
+
+  // Boton Siguiente
+  let btnSiguiente = document.createElement("button");
+  btnSiguiente.className = "btn btn-outline-primary";
+  btnSiguiente.innerHTML = "Siguiente &rightarrow;";
+  btnSiguiente.onclick = function () {
+    generaPagina(tipo, siguientePagina);
+  };
+
+  // Boton Final
+  let btnFinal = document.createElement("button");
+  btnFinal.className = "btn btn-primary";
+  btnFinal.innerHTML = "Final &rightarrowtail;";
+  btnFinal.onclick = function () {
+    generaPagina(tipo, info.pages);
+  };
+  bs.innerHTML = "";
+  bs.appendChild(btnInicio);
+  bs.appendChild(btnAnterior);
+  bs.appendChild(Label);
+  bs.appendChild(btnSiguiente);
+  bs.appendChild(btnFinal);
+};
+
+function hola(nombre) {
+  console.log("hola ", nombre);
+}
+
+const generaPagina = (tipo, page = 1) => {
+  getData(tipo, page).then((data) => {
+    limpiaDiv();
+    data.results.forEach((personaje) => {
+      creaPersonajeCard(personaje);
     });
+    creaBotones(data.info);
+  });
 };
-
-let getLugaresData = (callback) => {
-  fetch("https://rickandmortyapi.com/api/location")
-    .then((response) => response.json())
-    .then((data) => {
-      callback(data);
-    });
+const creaPersonajeCard = (personaje) => {
+  let div = document.createElement("div");
+  div.className = "col";
+  let codigo = `
+    <div class="card">
+      <img src="${personaje.image}" class="card-img-top" alt="${personaje.name}">
+      <div class="card-body">
+        <h5 class="card-title">${personaje.name}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+    `;
+  div.innerHTML = codigo;
+  let bs = document.querySelector("#lista");
+  bs.appendChild(div);
 };
-
-let getEpisodiosData = (callback) => {
-  fetch("https://rickandmortyapi.com/api/episode")
-    .then((response) => response.json())
-    .then((data) => callback(data));
-};
-
-export { getPersonajesData, getLugaresData, getEpisodiosData };
+export { generaPagina };
--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 console.log("Index.js Loaded");
-import {
-  getPersonajesData,
-  getEpisodiosData,
-  getLugaresData,
-} from "./funciones.js";
+import { generaPagina } from "./funciones.js";
 
-let personajes = getPersonajesData((data) => {
-  data.results.forEach((personaje) => {
-    console.log(personaje);
-    creaCard(personaje);
-  });
-});
-
-let lugares = getLugaresData((data) => {
-  console.log(data);
-});
-
-let episodios = getEpisodiosData((data) => {
-  console.log(data);
-});
-
-const creaCard = (personaje) => {
-  let li = document.createElement("li");
-  li.className = "list-group-item";
-  li.innerHTML = `<img src='${personaje.image}' class='fotoMini'> ${personaje.name}`;
-  let bs = document.querySelector("#lista");
-  bs.appendChild(li);
-};
+generaPagina('character');
